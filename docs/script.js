80365496bb28fd2921700d1954db66a4dd51a3e8
dudushy <eduardotalarico1337@gmail.com>
2023-01-18 12:18:42 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,12 @@
 console.log("[the-life-snake] init");
+
+const CONFIG = {
+  "initialSnakeLength": 3,
+  "initialSnakeSpeed": 1,
+  "initialSnakeDirection": "right", //? up, down, left, right
+  "initialSnakeColor": "#2dd36f", //? HEX
+  "mapWidth": 5,
+  "mapHeight": 5
+}
+
+
